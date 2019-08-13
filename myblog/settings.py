5ffef94eef2d236223a2fe051b897bce6793361f
vo0doo <kirsanov.bvt@gmail.com>
2019-08-13 09:28:51 +0300

    staticfiles ???

--- myblog/settings.py
+++ myblog/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Дополнительные места для collectstatic для поиска статических файлов.
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'staticfiles'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 # Обслуживание статических файлов
