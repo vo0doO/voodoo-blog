66ab3c4ea6e078ba5189a187ad515c53c965e34e
vo0doo <kirsanov.bvt@gmail.com>
2019-08-13 10:32:45 +0300

    heroku settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # # Дополнительные места для collectstatic для поиска статических файлов.
 #
